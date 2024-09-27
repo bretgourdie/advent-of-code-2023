44d13832a4f0efe012a8dd4d6a5d8d5da52b4956
Bret Gourdie <BretGourdie@gmail.com>
2024-09-27 12:55:49 -0400

    Solved Day 10 Part 1

---
+++ advent-of-code-2023/Day10/Day10.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using advent_of_code_2017;
+
+namespace advent_of_code_2023.Day10;
+internal class Day10 : AdventSolution
+{
+    private enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    private IDictionary<Direction, string> directionToPipe = new Dictionary<Direction, string>()
+    {
+        { Direction.North, "|7F" },
+        { Direction.South, "|LJ" },
+        { Direction.East, "-J7" },
+        { Direction.West, "-LF" }
+    };
+
+    private IDictionary<Direction, Vector2> directionToOffset = new Dictionary<Direction, Vector2>()
+    {
+        { Direction.North, new Vector2(0, -1) },
+        { Direction.South, new Vector2(0, 1) },
+        { Direction.West, new Vector2(-1, 0) },
+        { Direction.East, new Vector2(1, 0) }
+    };
+
+    private IDictionary<Direction, Direction> opposite = new Dictionary<Direction, Direction>()
+    {
+        { Direction.North, Direction.South },
+        { Direction.South, Direction.North },
+        { Direction.West, Direction.East },
+        { Direction.East, Direction.West }
+    };
+
+    protected override long part1Work(string[] input)
+    {
+        IList<Vector2> points = new List<Vector2>() { findStart(input) };
+        var grid = getFillableGrid(input);
+        long steps = 1;
+
+        while ((points = flood(input, points, grid, steps)).Any())
+        {
+            steps += 1;
+        }
+
+        return steps - 1;
+    }
+
+    private IList<Vector2> flood(
+        string[] input,
+        IList<Vector2> points,
+        long[][] grid,
+        long steps)
+    {
+        var newPoints = new List<Vector2>();
+
+        foreach (var point in points)
+        {
+            var x = (int)point.X;
+            var y = (int)point.Y;
+
+            foreach (var direction in directionToOffset.Keys)
+            {
+                var from = input[y][x];
+                var oppositeDirection = opposite[direction];
+                if (from == 'S' || directionToPipe[oppositeDirection].Contains(from))
+                {
+                    var offset = directionToOffset[direction];
+                    var xOffset = (int)offset.X;
+                    var yOffset = (int)offset.Y;
+
+                    var xNew = x + xOffset;
+                    var yNew = y + yOffset;
+
+                    if (0 <= yNew
+                        && yNew < input.Length 
+                        && 0 <= xNew
+                        && xNew < input[yNew].Length)
+                    {
+                        var nextPipe = input[yNew][xNew];
+
+                        if (directionToPipe[direction].Contains(nextPipe))
+                        {
+                            if (grid[yNew][xNew] == 0)
+                            {
+                                newPoints.Add(new Vector2(xNew, yNew));
+                                grid[yNew][xNew] = steps;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return newPoints;
+    }
+
+    private long[][] getFillableGrid(string[] input)
+    {
+        var grid = new long[input.Length][];
+
+        for (int ii = 0; ii < input.Length; ii++)
+        {
+            grid[ii] = new long[input[ii].Length];
+        }
+
+        return grid;
+    }
+
+    private Vector2 findStart(string[] input)
+    {
+        for (int y = 0; y < input.Length; y++)
+        {
+            for (int x = 0; x < input[y].Length; x++)
+            {
+                if (input[y][x] == 'S') return new Vector2(x, y);
+            }
+        }
+
+        throw new ArgumentException("S not found");
+    }
+
+    protected override long part1ExampleExpected => 8;
+    protected override long part1InputExpected => 6923;
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override long part2ExampleExpected { get; }
+    protected override long part2InputExpected { get; }
+}
