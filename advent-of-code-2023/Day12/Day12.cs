168df7de4700b245dda513f4c82b90cb32daf5fb
Bret Gourdie <BretGourdie@gmail.com>
2024-10-14 13:28:27 -0400

    Solved Day 12 Part 2, probably; unoptimized

--- advent-of-code-2023/Day12/Day12.cs
+++ advent-of-code-2023/Day12/Day12.cs
@@ -3,28 +3,60 @@
 namespace advent_of_code_2023.Day12;
 internal class Day12 : AdventSolution
 {
-    private const string unknown = "?";
+    private const char unknown = '?';
     private const char operational = '.';
     private const char damaged = '#';
     private static IList<char> replacements = new List<char>() { operational, damaged };
 
-    protected override long part1Work(string[] input)
+    protected override long part1Work(string[] input) =>
+        work(input, 1);
+
+    private long work(
+        string[] input,
+        int numberOfExpansions)
     {
         long permutations = 0;
 
         foreach (var line in input)
         {
+            var record = expandRecord(getRecord(line), numberOfExpansions);
+
             permutations += getPermutations(
-                getRecord(line),
-                getPattern(line));
+                record,
+                expandPattern(getPattern(line), numberOfExpansions),
+                record
+            );
         }
 
         return permutations;
     }
 
+    private string expandRecord(
+        string start,
+        int numberOfExpansions)
+    {
+        return
+            String.Join(unknown,
+                Enumerable.Repeat(
+                    start,
+                    numberOfExpansions)
+            );
+    }
+
+    private IList<int> expandPattern(
+        IList<int> start,
+        int numberOfExpansions)
+    {
+        return
+            Enumerable.Repeat(start, numberOfExpansions)
+                .SelectMany(x => x)
+                .ToList();
+    }
+
     private long getPermutations(
         string record,
-        IList<int> pattern)
+        IList<int> pattern,
+        string originalRecord)
     {
         if (record.Contains(unknown))
         {
@@ -37,45 +69,88 @@ internal class Day12 : AdventSolution
                 chars[unknownIndex] = replacement;
                 var newRecord = new string(chars);
 
-                permutations += getPermutations(newRecord, pattern);
+                if (patternWorks(newRecord, pattern))
+                {
+                    permutations += getPermutations(
+                        newRecord,
+                        pattern,
+                        originalRecord);
+                }
             }
 
             return permutations;
         }
 
+        if (patternWorks(record, pattern))
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private bool patternWorks(string record, IList<int> pattern)
+    {
         var queue = new Queue<int>(pattern);
 
-        int patternIndex = 0;
-        while (queue.Any())
+        int recordLetterIndex = 0;
+        while (queue.Any()
+                && recordLetterIndex < record.Length
+                && record[recordLetterIndex] != unknown)
         {
             var groupGoal = queue.Dequeue();
             var groupCount = 0;
 
-            while (patternIndex < record.Length && record[patternIndex] != damaged)
+            while (recordLetterIndex < record.Length
+                   && record[recordLetterIndex] != damaged
+                   && record[recordLetterIndex] != unknown)
             {
-                patternIndex += 1;
+                recordLetterIndex += 1;
             }
 
-            if (patternIndex == record.Length) return 0;
-
-            while (patternIndex < record.Length && record[patternIndex] == damaged)
+            if (recordLetterIndex == record.Length)
             {
-                patternIndex += 1;
+                return false;
+            }
+
+            while (recordLetterIndex < record.Length
+                   && record[recordLetterIndex] == damaged
+                   && record[recordLetterIndex] != unknown)
+            {
+                recordLetterIndex += 1;
                 groupCount += 1;
+            }
+
+            if (recordLetterIndex == record.Length)
+            {
+                if (groupCount != groupGoal)
+                {
+                    return false;
+                }
+            }
+
+            else if (record[recordLetterIndex] == unknown && groupCount <= groupGoal)
+            {
+                return true;
             }
 
             if (groupCount != groupGoal)
             {
-                return 0;
+                return false;
             }
         }
 
-        if (patternIndex + 1 < record.Length && record.Substring(patternIndex + 1).Contains(damaged))
+        if (queue.Any())
         {
-            return 0;
+            return false;
         }
 
-        return 1;
+        if (recordLetterIndex + 1 < record.Length && record.Substring(recordLetterIndex + 1).Contains(damaged))
+        {
+            return false;
+        }
+
+        return true;
     }
 
     private string getRecord(string line)
@@ -95,12 +170,11 @@ internal class Day12 : AdventSolution
     }
 
     protected override long part1ExampleExpected => 21;
-    protected override long part1InputExpected => -1;
-    protected override long part2Work(string[] input)
-    {
-        throw new NotImplementedException();
-    }
+    protected override long part1InputExpected => 8419;
 
-    protected override long part2ExampleExpected { get; }
-    protected override long part2InputExpected { get; }
+    protected override long part2Work(string[] input) =>
+        work(input, 5);
+
+    protected override long part2ExampleExpected => 525152;
+    protected override long part2InputExpected => -1;
 }
