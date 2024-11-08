312d2c30c2bfd71d2c29d27ff7b976919e6af03c
Bret Gourdie <BretGourdie@gmail.com>
2024-11-08 12:00:13 -0500

    Solved Day 13 Part 1

---
+++ advent-of-code-2023/Day13/Day13.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using advent_of_code_2017;
+
+namespace advent_of_code_2023.Day13;
+internal class Day13 : AdventSolution
+{
+    protected override long part1Work(string[] input)
+    {
+        var grids = getGrids(input);
+        long total = 0;
+
+        foreach (var grid in grids)
+        {
+            var result = grid.FindSymmetry();
+            total += result;
+        }
+
+        return total;
+    }
+
+    private IList<Grid> getGrids(string[] input)
+    {
+        var listOfGrids = new List<Grid>();
+
+        var gridBuildup = new List<string>();
+
+        foreach (var line in input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                listOfGrids.Add(
+                    new Grid(
+                        gridBuildup.ToArray()
+                    )
+                );
+                gridBuildup = new List<string>();
+            }
+
+            else
+            {
+                gridBuildup.Add(line);
+            }
+        }
+
+        listOfGrids.Add(new Grid(gridBuildup.ToArray()));
+
+        return listOfGrids;
+    }
+
+    protected override long part1ExampleExpected => 405;
+    protected override long part1InputExpected => 29165;
+    protected override long part2Work(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override long part2ExampleExpected { get; }
+    protected override long part2InputExpected { get; }
+}
---
+++ advent-of-code-2023/Day13/Grid.cs
@@ -0,0 +1,126 @@
+﻿namespace advent_of_code_2023.Day13;
+internal class Grid
+{
+    public readonly string[] TheGrid;
+
+    public Grid(string[] grid)
+    {
+        TheGrid = grid;
+    }
+
+    public long FindSymmetry()
+    {
+        var result = findSymmetry(TheGrid);
+
+        if (result.Line == Result.SymmetryType.Horizontal)
+        {
+            return getHorizontalLineScore(result.LeftoverLines);
+        }
+
+        else if (result.Line == Result.SymmetryType.Vertical)
+        {
+            return getVerticalLineScore(result.LeftoverLines);
+        }
+
+        throw new NotImplementedException("No symmetry");
+    }
+
+    private Result findSymmetry(string[] grid)
+    {
+        var rows = checkHorizontal(grid);
+        if (rows > 0)
+        {
+            return new Result(Result.SymmetryType.Horizontal, rows);
+        }
+
+        var columns = checkVertical(grid);
+        if (columns > 0)
+        {
+            return new Result(Result.SymmetryType.Vertical, columns);
+        }
+
+        throw new NotImplementedException("Could not find symmetry");
+    }
+
+    private int checkHorizontal(string[] grid)
+    {
+        var rows = grid.Length;
+        var columns = grid.First().Length;
+
+        for (int southOfLine = 1; southOfLine < rows; southOfLine++)
+        {
+            bool symmetry = true;
+
+            for (int offset = 0; southOfLine - offset - 1 >= 0 && southOfLine + offset < rows; offset++)
+            {
+                var northLine = grid[southOfLine - offset - 1];
+                var southLine = grid[southOfLine + offset];
+
+                for (int letterIndex = 0; letterIndex < columns; letterIndex++)
+                {
+                    symmetry &= northLine[letterIndex] == southLine[letterIndex];
+                }
+            }
+
+            if (symmetry)
+            {
+                return southOfLine;
+            }
+        }
+
+        return 0;
+    }
+
+    private int checkVertical(string[] grid)
+    {
+        var rows = grid.Length;
+        var columns = grid.First().Length;
+
+        for (int eastOfLine = 1; eastOfLine < columns; eastOfLine++)
+        {
+            bool symmetry = true;
+
+            for (int offset = 0; eastOfLine - offset - 1 >= 0 && eastOfLine + offset < columns; offset++)
+            {
+                for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+                {
+                    var westLetter = grid[rowIndex][eastOfLine - offset - 1];
+                    var eastLetter = grid[rowIndex][eastOfLine + offset];
+
+                    symmetry &= westLetter == eastLetter;
+                }
+            }
+
+            if (symmetry)
+            {
+                return eastOfLine;
+            }
+        }
+
+        return 0;
+    }
+
+    private long getHorizontalLineScore(int rowsAbove) =>
+        rowsAbove * 100;
+
+    private long getVerticalLineScore(int columnsToTheLeft) =>
+        columnsToTheLeft * 1;
+
+    private struct Result
+    {
+        public enum SymmetryType
+        {
+            Horizontal,
+            Vertical
+        }
+
+        public readonly SymmetryType Line;
+        public int LeftoverLines;
+
+        public Result(SymmetryType line, int leftoverLines)
+        {
+            Line = line;
+            LeftoverLines = leftoverLines;
+        }
+    }
+}
